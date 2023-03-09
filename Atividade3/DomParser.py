f2ff60b95495fdf6091f05a19b64ca98783e9d4e
messiasjs <mjls.junior.js@gmail.com>
2023-03-09 10:08:21 -0300

    corrigindo

--- Atividade3/DomParser.py
+++ Atividade3/DomParser.py
@@ -3,21 +3,25 @@ import time
 
 inicio = time.time()
 OSMDocument = parse('map.osm')
-amenity = False
+amenity = None
+name = None
 count =0
 t=""
 print("Starting DOM Parser...")
 for node in OSMDocument.getElementsByTagName("node"):	
 	for tag in  node.getElementsByTagName("tag"):
 		if tag.getAttribute("k") == "amenity":
-			amenity =True
-			t = tag.getAttribute("v")
-		if tag.getAttribute("k") == "name" and amenity:
-			count +=1
-			print("Nome:", tag.getAttribute("v"), end="\t")
-			print("Tipo: ", t, end="\t")
-			print("Latitude: ", node.getAttribute("lat"), end="\t")
-			print("Longitude: ", node.getAttribute("lon")) 
+			amenity = tag.getAttribute("v")
+			#t = tag.getAttribute("v")
+		if tag.getAttribute("k") == "name":
+			name = tag.getAttribute("v")
+	if name and amenity:
+		count +=1
+		print("Nome:", name, end="\t")
+		print("Tipo: ", amenity, end="\t")
+		print("Latitude: ", node.getAttribute("lat"), end="\t")
+		print("Longitude: ", node.getAttribute("lon")) 
 	amenity=False
+	nome = None
 fim = time.time()
 print("DOM time:", fim - inicio)
