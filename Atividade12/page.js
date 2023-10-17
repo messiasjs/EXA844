f3c8ce862324e3dfdc900df8dc8e5318271a903a
messiasjs <mjls.junior.js@gmail.com>
2023-10-17 10:13:50 -0300

    Atividade 11 e 12

---
+++ Atividade12/page.js
@@ -0,0 +1,26 @@
+"use client"; 
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import React from 'react';
+import { useState } from 'react';
+
+
+//Coloque o código dos demais componentes aqui...
+
+export default function Home() {
+    
+  const [blogMessages, setBlogMessages] = useState([]);
+  
+  fetch('https://...')
+    .then(response => response.json())
+    .then(data => {
+        setBlogMessages(data);
+    });
+    
+    return (
+      <main className={styles.main}>
+        <FilterableMessageTable messages={blogMessages} />
+      </main>
+    )
+}
